ccc72f574ada204ea4d8dc3704eac787dd9dca84
relliky <relliky@hotmail.com>
2014-11-15 18:44:10 +0000

    remove duplicated tivaware

--- CCS_projects/Tivaware_driverlib_css/ccs/Debug/cpu.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-cpu.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/cpu.c
-cpu.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.7/include/stdint.h
-cpu.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/cpu.h
-
-C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/cpu.c: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.7/include/stdint.h: 
-C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/cpu.h: 
--- CCS_projects/Tivaware_driverlib_css/ccs/Debug/sw_crc.pp
+++
@@ -1,9 +0,0 @@
-# FIXED
-
-sw_crc.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sw_crc.c
-sw_crc.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.7/include/stdint.h
-sw_crc.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sw_crc.h
-
-C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sw_crc.c: 
-c:/ti/ccsv6/tools/compiler/arm_5.1.7/include/stdint.h: 
-C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sw_crc.h: 
